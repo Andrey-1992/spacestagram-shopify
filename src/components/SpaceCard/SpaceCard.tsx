9988dd157560201bf0123dcb6528680949a1df79
Andrey Bermudez <andrey.bermudez92@gmail.com>
2022-01-18 23:07:10 -0600

    Set up explanation process for the logic and flow of the pictures

--- src/components/SpaceCard/SpaceCard.tsx
+++ src/components/SpaceCard/SpaceCard.tsx
@@ -11,18 +11,26 @@ interface Props {
 }
 
 export const SpaceCard: React.FC<Props> = ({ key, id, title , date, url, explanation}) => {
-  const [isFavorite, setIsFavorite] = useState<boolean>(false)
+  const [isFavorite, setIsFavorite] = useState<boolean>(false);
+  const [showExplanation, setShowExplanation ] = useState<boolean>(false);
 
   return (
     <div className='space-card' key={key}>
       <img className='space-image'  src={url} alt={title}/>
       <h2 className='pic-title'>{title}</h2>
       <h2 className='capture-date'>{date}</h2>
-      {explanation}
       <button
-      className='like-button'
-      onClick={() => setIsFavorite(!isFavorite) }
-      >{isFavorite ? 'Liked!' : 'Like'}</button>
+        className='like-button'
+        onClick={() => setIsFavorite(!isFavorite) }
+      >{isFavorite ? 'Liked!' : 'like' }</button>
+      <button 
+        className="show-img-explanation" onClick={() => setShowExplanation(!showExplanation)}
+      >{!showExplanation? '+' : '-'}</button>
+      {showExplanation && 
+        <p className='img-explanation'>
+          {explanation}
+        </p>
+      }
     </div>
   );
 }
