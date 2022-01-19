443eaedbdfedd23967da8b9f4df880897c9bae0b
Andrey Bermudez <andrey.bermudez92@gmail.com>
2022-01-19 00:22:56 -0600

    Set up layout and stykling for loading component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -38,8 +38,9 @@ export const App: React.FC = () => {
         className='fetch-data-btn'
         onClick={() => getTodayData()}
       >Search more pictures</button>
-      { isLoading && <Loading /> }
-      <SpaceCardContainer nasaApi={nasaData}/>
+      {/* { isLoading && <Loading /> } */}
+      {/* <SpaceCardContainer nasaApi={nasaData}/> */}
+      <Loading />
     </div>
   );
 }
