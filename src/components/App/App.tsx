ceeaea9aa1e9c9682809e78be182785e8cf47af9
Andrey Bermudez <andrey.bermudez92@gmail.com>
2022-01-18 21:54:05 -0600

    Set up the types for the api call data

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,6 +5,16 @@ import { Loading } from '../Loading/Loading';
 import { fetchSpaceContainerImg, fetchTodaysPic } from '../Util/ApiCalls';
 import { useState, useEffect } from 'react';
 
+interface nasaPicTodayInfo {
+  date: string
+  explanation: string
+  hdurl: string
+  media_type: string
+  service_version: string
+  title: string
+  url: string
+  copyright: string
+}
 
 export const App: React.FC = () => {
   const [spaceData, setSpaceData] = useState<any>([]);
