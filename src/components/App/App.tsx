e9ae0ef80b094afcef6bac646462abb30c4d4f03
Andrey Bermudez <andrey.bermudez92@gmail.com>
2022-01-18 22:01:35 -0600

    Set up a get data function for initial function

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,20 +6,29 @@ import { fetchSpaceContainerImg, fetchTodaysPic } from '../Util/ApiCalls';
 import { useState, useEffect } from 'react';
 
 interface nasaPicTodayInfo {
-  date: string
-  explanation: string
-  hdurl: string
-  media_type: string
-  service_version: string
-  title: string
-  url: string
-  copyright: string
+  date?: string
+  explanation?: string
+  hdurl?: string
+  media_type?: string
+  service_version?: string
+  title?: string
+  url?: string
+  copyright?: string
 }
 
 export const App: React.FC = () => {
   const [spaceData, setSpaceData] = useState<any>([]);
   const [error, setError] = useState<string>('');
   const [isLoading, setIsLoading ] = useState<boolean>(true);
+
+  
+
+  const getData = async () => {
+    fetchTodaysPic()
+    // .then(data => console.log(data))
+    .then(data => spaceData(data))
+    .catch(error => console.log(error))
+  }
 
   return (
     <div className="App">
