0ed72ad394504b4c74264847d3aa7768c22a8673
Andrey Bermudez <andrey.bermudez92@gmail.com>
2022-01-18 21:39:57 -0600

    Create a fetch call for nasa picture of the day fetch call

--- src/components/Util/ApiCalls.tsx
+++ src/components/Util/ApiCalls.tsx
@@ -11,3 +11,17 @@ export const fetchSpaceContainerImg = async () => {
     return err;
   }
 }
+
+export const fetchTodaysPic = async () => {
+  try {
+    const response = await fetch("https://api.nasa.gov/planetary/apod?api_key=iO71ydirnMQL3x20vdpVLigRiB3uytZaCgxG23z5")
+    if (!response.ok || response.status === 500) {
+      throw Error('Error fetching NASA picture of the day')
+    }
+    const data = await response.json()
+    return data
+  } catch(err) {
+    console.log(err.message)
+    return err;
+  }
+}
