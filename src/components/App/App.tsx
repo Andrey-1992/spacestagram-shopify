85e4dde07c533e1f655fba7abe38e224b401716b
Andrey Bermudez <andrey.bermudez92@gmail.com>
2022-01-18 21:27:50 -0600

    Change tab title on the browser

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-// import './App.css';
+import './App.css';
 
 export const App = () => {
   return (
     <div className="App">
+      <h1 className='header'>Spacestagram</h1>
+      <h2 className='subheader'>Brought to you by NASA's image API</h2>
     </div>
   );
 }
